e6d921334429f5cd06e7e7e49ad1a559f3ab3631
Nabby12 <Nabby12@users.noreply.github.com>
2020-10-30 14:49:29 +0900

    added/post-twitter-function

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,19 +1,27 @@
-
 const twitter = require('twitter');
 
+const TWITTER_API_KEY = process.env['API_KEY']
+const TWITTER_API_KEY_SECRET = process.env['API_KEY_SECRET']
+const TWITTER_ACCESS_TOKEN = process.env['ACCESS_TOKEN']
+const TWITTER_ACCESS_TOKEN_SECRET = process.env['ACCESS_TOKEN_SECRET']
+
 const twitter_client = new twitter({
-    consumer_key        : process.env['API_KEY'],
-    consumer_secret     : process.env['API_KEY_SECRET'],
-    access_token_key    : process.env['ACCESS_TOKEN'],
-    access_token_secret : process.env['ACCESS_TOKEN_SECRET'],
+    consumer_key        : TWITTER_API_KEY,
+    consumer_secret     : TWITTER_API_KEY_SECRET,
+    access_token_key    : TWITTER_ACCESS_TOKEN,
+    access_token_secret : TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-exports.handler = async () => {
-    // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = process.env['TWITTER_API_KEY_SECRET'];
+exports.handler = () => {
+    const postContent = 'test投稿'
+    const params = { status: postContent };
 
-    // All log statements are written to CloudWatch
-    console.info(`${message}`);
-    
-    return message;
+    twitter_client.post('statuses/update', params, (error, tweet) => {
+        if(error) {
+            console.log(error);
+            return
+        } else {
+            console.log('投稿に成功しました。');
+        }
+    });
 }
