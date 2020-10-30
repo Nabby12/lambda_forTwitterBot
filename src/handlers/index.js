306f9c8ae637cef07e94c5bf2b2cf632a984cb4b
Nabby12 <Nabby12@users.noreply.github.com>
2020-10-30 13:25:16 +0900

    modified/environmet-variables

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,10 @@
-/**
- * A Lambda function that returns a static string
- */
+const TWITTER_API_KEY = process.env['TWITTER_API_KEY'];
+const TWITTER_API_KEY_SECRET = process.env['TWITTER_API_KEY_SECRET'];
+const BEARER_TOKEN = process.env['BEARER_TOKEN'];
+
 exports.handler = async () => {
     // If you change this message, you will need to change hello-from-lambda.test.js
-    const message = process.env['SAMPLE_VALUE'];
+    const message = process.env['TWITTER_API_KEY_SECRET'];
 
     // All log statements are written to CloudWatch
     console.info(`${message}`);
